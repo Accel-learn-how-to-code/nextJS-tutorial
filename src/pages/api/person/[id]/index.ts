f1336985ce6b0c4f5684bf788045cd58af28c6f8
Accel <58154200+Accel-learn-how-to-code@users.noreply.github.com>
2021-04-02 17:05:12 +0700

    add PUT method

--- src/pages/api/person/[id]/index.ts
+++ src/pages/api/person/[id]/index.ts
@@ -11,6 +11,30 @@ export default async function getPersonById(
     driver: sqlite3.Database,
   });
 
+  if (req.method === "PUT") {
+    // const statement = await db.prepare(
+    //   "UPDATE Person SET name=?, email=? where id=?"
+    // );
+    // const result = await statement.run(
+    //   req.body.name,
+    //   req.body.email,
+    //   req.query.id
+    // );
+
+    const statement = await db.prepare(
+      "UPDATE Person SET name=?, email=? where id=?"
+    );
+    await statement.bind(req.body.name, req.body.email, req.query.id);
+    let result = await statement.get();
+
+    // const result = await db.run(
+    //   "UPDATE Person SET name=?, email=? where id=?",
+    //   req.body.name,
+    //   req.body.email,
+    //   req.query.id
+    // );
+  }
+
   const person = await db.get("SELECT * FROM Person where id = ?", [
     req.query.id,
   ]);
